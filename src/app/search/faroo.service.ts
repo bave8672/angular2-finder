f6281a641028fc7d6cbaba9280a7d8bd3d1ddc05
Ben Aves <avesbenjamin@gmail.com>
2017-04-30 21:20:32 +1000

    move to firebase hosting

--- src/app/search/faroo.service.ts
+++ src/app/search/faroo.service.ts
@@ -3,7 +3,7 @@ import {Injectable} from 'angular2/core';
 @Injectable()
 export class Faroo implements app.Faroo.Constants {
 
-	apiBase = 'http://www.faroo.com/api?key=TNGxHOF3ztMgyEgShLLSudOqytQ_';
+	apiBase = 'https://www.faroo.com/api?key=TNGxHOF3ztMgyEgShLLSudOqytQ_';
 
 	sources = {
 		web: 'web',
